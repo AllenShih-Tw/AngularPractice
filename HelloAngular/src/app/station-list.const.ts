2dc076af6fec7a0951966c5659e7661cfbc8d807
Allen <allendxbest84@gmail.com>
2021-10-24 16:14:27 +0800

    Class 1~10

---
+++ HelloAngular/src/app/station-list.const.ts
@@ -0,0 +1,17 @@
+export const stationList = [
+        {
+          "Station":"南港展覽館站1",
+          "Destination":"頂埔站",
+          "UpdateTime":"2015-07-20T18:25:56.927"
+        },
+        {
+            "Station":"南港展覽館站2",
+            "Destination":"頂埔站",
+            "UpdateTime":"2015-07-20T18:25:56.927"
+          },
+          {
+            "Station":"南港展覽館站3",
+            "Destination":"頂埔站",
+            "UpdateTime":"2015-07-20T18:25:56.927"
+          }
+];
